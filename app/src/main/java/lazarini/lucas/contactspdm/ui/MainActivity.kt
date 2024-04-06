f3b05c53e5f201489e92f216401da6585a5ca0d7
corrumptus <lazarinilucas42@gmail.com>
2024-04-05 21:33:03 -0300

    creating the list of contacts, filling it and adding to the list view by the list adapter

--- app/src/main/java/lazarini/lucas/contactspdm/ui/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/contactspdm/ui/MainActivity.kt
@@ -1,16 +1,55 @@
 package lazarini.lucas.contactspdm.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import lazarini.lucas.contactspdm.R
 import lazarini.lucas.contactspdm.databinding.ActivityMainBinding
+import lazarini.lucas.contactspdm.model.Contact
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    // DATA SOURCE
+    private val contactList: MutableList<Contact> = mutableListOf()
+
+//    // ADAPTER
+    private val listAdapter: ArrayAdapter<Contact> by lazy {
+        ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        amb.toolbarIn.apply {
+            setSupportActionBar(this.toolbar)
+        }
+
+        fillList()
+
+        amb.contactsLv.adapter = listAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    private fun fillList() {
+        for (i in 1..50) {
+            contactList.add(
+                Contact(
+                    i,
+                    "name $i",
+                    "address $i",
+                    "phone $i",
+                    "email $i"
+                )
+            )
+        }
     }
 }
