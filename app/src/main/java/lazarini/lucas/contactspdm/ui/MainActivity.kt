1fe8045268ba7db266c3b73df7992c8947699f6a
corrumptus <lazarinilucas42@gmail.com>
2024-04-05 23:07:22 -0300

    changing the list filler loop function to a message between the main activity and the contact view creator contact activity

--- app/src/main/java/lazarini/lucas/contactspdm/ui/MainActivity.kt
+++ app/src/main/java/lazarini/lucas/contactspdm/ui/MainActivity.kt
@@ -1,22 +1,30 @@
 package lazarini.lucas.contactspdm.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.contactspdm.R
 import lazarini.lucas.contactspdm.databinding.ActivityMainBinding
 import lazarini.lucas.contactspdm.model.Contact
+import lazarini.lucas.contactspdm.utils.IntentMapper.CREATE_CONTACT
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var carl: ActivityResultLauncher<Intent>
+
     // DATA SOURCE
     private val contactList: MutableList<Contact> = mutableListOf()
 
-//    // ADAPTER
+    // ADAPTER
     private val listAdapter: ArrayAdapter<Contact> by lazy {
         ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList)
     }
@@ -29,7 +37,32 @@ class MainActivity : AppCompatActivity() {
             setSupportActionBar(this.toolbar)
         }
 
-        fillList()
+        carl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                result.data?.getParcelableExtra<Contact>(CREATE_CONTACT)?.let {
+                    if (!isContactValid(it)) {
+                        Toast.makeText(
+                            this,
+                            "Invalid contact. All the attributes cannot be blank",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        return@let
+                    }
+
+                    it.apply {
+                        id = if (contactList.isEmpty()) 1 else contactList.lastIndex+1
+                    }
+
+                    contactList.add(it)
+
+                    listAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
+//        fillList()
 
         amb.contactsLv.adapter = listAdapter
     }
@@ -39,17 +72,32 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    private fun fillList() {
-        for (i in 1..50) {
-            contactList.add(
-                Contact(
-                    i,
-                    "name $i",
-                    "address $i",
-                    "phone $i",
-                    "email $i"
-                )
-            )
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Intent(this, ContactActivity::class.java).also {
+            carl.launch(it)
         }
+
+        return true
     }
+
+    private fun isContactValid(contact: Contact): Boolean {
+        return contact.name.isNotBlank() &&
+            contact.address.isNotBlank() &&
+            contact.phone.isNotBlank() &&
+            contact.email.isNotBlank()
+    }
+
+//    private fun fillList() {
+//        for (i in 1..50) {
+//            contactList.add(
+//                Contact(
+//                    i,
+//                    "name $i",
+//                    "address $i",
+//                    "phone $i",
+//                    "email $i"
+//                )
+//            )
+//        }
+//    }
 }
