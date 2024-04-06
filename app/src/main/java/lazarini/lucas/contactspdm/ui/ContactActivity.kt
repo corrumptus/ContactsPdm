71de15d4e3f3008ec7a9c540c65a7dc5f04292e9
corrumptus <lazarinilucas42@gmail.com>
2024-04-05 21:57:42 -0300

    adding the result sender of the view

--- app/src/main/java/lazarini/lucas/contactspdm/ui/ContactActivity.kt
+++ app/src/main/java/lazarini/lucas/contactspdm/ui/ContactActivity.kt
@@ -1,8 +1,11 @@
 package lazarini.lucas.contactspdm.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.contactspdm.databinding.ActivityContactBinding
+import lazarini.lucas.contactspdm.model.Contact
+import lazarini.lucas.contactspdm.utils.IntentMapper.CREATE_CONTACT
 
 class ContactActivity : AppCompatActivity() {
     private val acb: ActivityContactBinding by lazy {
@@ -15,6 +18,26 @@ class ContactActivity : AppCompatActivity() {
 
         acb.toolbarIn.apply {
             setSupportActionBar(this.toolbar)
+        }
+
+        acb.enviaBt.setOnClickListener {
+            val name = acb.nameEt.text.toString()
+            val address = acb.addressEt.text.toString()
+            val phone = acb.phoneEt.text.toString()
+            val email = acb.emailEt.text.toString()
+
+            val resultadoIntent = Intent()
+
+            resultadoIntent.putExtra(CREATE_CONTACT,
+                Contact(
+                    name = name,
+                    address = address,
+                    phone = phone,
+                    email = email
+                )
+            )
+            setResult(RESULT_OK, resultadoIntent)
+            finish()
         }
     }
 }
