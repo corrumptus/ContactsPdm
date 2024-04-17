85dbbea6ec8e981b0534e64f2d02986b7673eace
corrumptus <lazarinilucas42@gmail.com>
2024-04-17 14:48:53 -0300

    adding a new type of intent message

--- app/src/main/java/lazarini/lucas/contactspdm/utils/IntentMapper.kt
+++ app/src/main/java/lazarini/lucas/contactspdm/utils/IntentMapper.kt
@@ -1,6 +1,7 @@
 package lazarini.lucas.contactspdm.utils
 
 object IntentMapper {
+    const val VIEW_CONTACT = "VIEW_CONTACT"
     const val CREATE_CONTACT = "CREATE_CONTACT"
     const val EDIT_CONTACT = "EDIT_CONTACT"
 }
