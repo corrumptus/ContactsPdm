009c877d415d9e39250ce9c59f55d60cca8a7043
corrumptus <lazarinilucas42@gmail.com>
2024-04-17 14:55:03 -0300

    changing the contact activity layout when the the contact is in the view mode and setting its properties in the view

--- app/src/main/java/lazarini/lucas/contactspdm/ui/ContactActivity.kt
+++ app/src/main/java/lazarini/lucas/contactspdm/ui/ContactActivity.kt
@@ -2,11 +2,13 @@ package lazarini.lucas.contactspdm.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import lazarini.lucas.contactspdm.databinding.ActivityContactBinding
 import lazarini.lucas.contactspdm.model.Contact
 import lazarini.lucas.contactspdm.utils.IntentMapper.CREATE_CONTACT
 import lazarini.lucas.contactspdm.utils.IntentMapper.EDIT_CONTACT
+import lazarini.lucas.contactspdm.utils.IntentMapper.VIEW_CONTACT
 
 class ContactActivity : AppCompatActivity() {
     private val acb: ActivityContactBinding by lazy {
@@ -50,6 +52,22 @@ class ContactActivity : AppCompatActivity() {
 
             setResult(RESULT_OK, resultIntent)
             finish()
+        }
+
+        intent.getParcelableExtra<Contact>(VIEW_CONTACT)?.apply {
+            acb.enviaBt.visibility = View.GONE
+
+            acb.nameEt.setText(this.name)
+            acb.nameEt.isEnabled = false
+
+            acb.addressEt.setText(this.address)
+            acb.addressEt.isEnabled = false
+
+            acb.phoneEt.setText(this.phone)
+            acb.phoneEt.isEnabled = false
+
+            acb.emailEt.setText(this.email)
+            acb.emailEt.isEnabled = false
         }
 
         intent.getParcelableExtra<Contact>(EDIT_CONTACT)?.apply {
