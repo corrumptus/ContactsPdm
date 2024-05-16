170547c4d0afb09594d8f35b77f2766f343fab5b
corrumptus <lazarinilucas42@gmail.com>
2024-05-15 21:15:26 -0300

    creating a sqlite db in the context

--- app/src/main/java/lazarini/lucas/contactspdm/persistence/ContactDAOImplSqlite.kt
+++ app/src/main/java/lazarini/lucas/contactspdm/persistence/ContactDAOImplSqlite.kt
@@ -1,8 +1,17 @@
 package lazarini.lucas.contactspdm.persistence
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.database.sqlite.SQLiteDatabase
 import lazarini.lucas.contactspdm.model.Contact
 
-class ContactDAOImplSqlite: ContactDAO {
+class ContactDAOImplSqlite(context: Context): ContactDAO {
+    private val contactSqliteDatabase: SQLiteDatabase = context.openOrCreateDatabase(
+        "contacts",
+        MODE_PRIVATE,
+        null
+    )
+
     override fun createContact(contact: Contact): Int {
         TODO("Not yet implemented")
     }
